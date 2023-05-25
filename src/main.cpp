c77576cd8c76ecf11a9d4af394a85338c1bde306
Prince-Hervoet <2064128441@qq.com>
2023-05-25 15:26:11 +0000

    fix: #add codes

--- src/main.cpp
+++ src/main.cpp
@@ -1,11 +1,24 @@
 #include <iostream>
 #include "dynamic_object.hpp"
 
+etools::DynamicObject &test()
+{
+    etools::DynamicObject &dy = etools::getDynamicObject(true);
+    int *res = new int[10];
+    double s = 1.2;
+    dy.Add(res);
+    dy.Add(s);
+    dy.Add(123124);
+    return dy;
+}
+
 int main()
 {
-    etools::DynamicObject dy = etools::getDynamicObject(true);
-    std::string s = "1234";
-    dy.Set(s, 340);
-    dy.Add("asdfasdf");
-    std::cout << dy[0]->GetIntValue() << std::endl;
+    etools::DynamicObject dy = test();
+    std::cout << (int *)dy[0]->GetPtrValue() << std::endl;
+    std::cout << dy[1]->GetDoubleValue() << std::endl;
+    delete[] (int *)dy[0]->GetPtrValue();
+    dy.Clear();
+    std::cout << sizeof(dy) << std::endl;
+    return 0;
 }
